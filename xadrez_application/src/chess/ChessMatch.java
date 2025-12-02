7097f76588b6bbc24978fccab3fb2b411d06bd2d
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2025-12-02 16:38:03 -0300

    Possiveis movimentos torre

--- xadrez_application/src/chess/ChessMatch.java
+++ xadrez_application/src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        ValidateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece)capturedPiece;
     }
@@ -49,10 +50,18 @@ public class ChessMatch {
         }
     }
 
+    private void ValidateTargetPosition(Position source, Position target){
+        if (!board.piece(source).possibleMove(target)){
+            throw new ChessException("The chosen piece can´t move to target position");
+        }
+    }
+
 
     private void placeNewPiece(char column, int row, ChessPiece piece ){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
+
+
 
 
     // metodo responsável por iniciar a partida colocando as peças no tabuleiro
